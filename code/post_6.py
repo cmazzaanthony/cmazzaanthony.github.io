ea95b0ddbfe16ba1178ef1ab07fbf7b09d885f90
cmazzaanthony <cmazzaanthony@gmail.com>
2020-04-12 14:52:44 -0400

    Add more details to blog post 6

--- code/post_6.py
+++ code/post_6.py
@@ -1,1 +1,21 @@
 # PDF post
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+burps_per_episode = [20, 19, 14, 13, 12, 11, 11, 11, 10, 8, 8, 7, 6, 5, 4, 3, 3, 2, 2, 1, 1]
+mean_burps_per_episode = np.array(burps_per_episode).mean().round(1)
+variance_burps_per_episode = np.array(burps_per_episode).var().round(1)
+
+print(mean_burps_per_episode)
+print(variance_burps_per_episode)
+
+# 1. Pick a distribution, easiest is Gaussian, use mean and variance
+
+# sns.distplot(burps_per_episode, hist=True, kde=True,
+#              bins=int(180/5), color = 'darkblue',
+#              hist_kws={'edgecolor':'black'},
+#              kde_kws={'linewidth': 4})
+#
+# plt.show()

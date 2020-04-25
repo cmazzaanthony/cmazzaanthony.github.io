1f10178e8a2b3c68ee1fef1d7bcddc09f7397c1a
cmazzaanthony <cmazzaanthony@gmail.com>
2020-04-25 11:12:53 -0400

    Added pmf for moments

--- code/post_7.py
+++ code/post_7.py
@@ -1,1 +1,21 @@
-# Moments: Expectation and Variance
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+probabilities = pd.DataFrame(
+    data=[
+        [0, 0.2],
+        [1, 0.1],
+        [2, 0.5],
+        [3, 0.2],
+    ],
+    columns=['The number of times Morty says \'Ah Jeez\'', 'Probability']
+)
+
+ax = sns.barplot(
+    x='The number of times Morty says \'Ah Jeez\'',
+    y='Probability',
+    data=probabilities,
+)
+plt.savefig('pmf_moments')
